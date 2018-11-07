f665a68245b5d62875632871310ce33ecf970f66
ltcptgeneral <35508619+ltcptgeneral@users.noreply.github.com>
2018-11-06 22:08:43 -0600

    commit

--- analysis_test.py
+++ analysis_test.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import analysis
 
 data = analysis.load_csv('data.txt')
@@ -27,17 +27,3 @@ game_objectives.append("auto", 3, [0, 10], "1, 10")
 game_objectives.edit(3, "null", 4, "null", "null")
 print(game_objectives.search(4))
 print(game_objectives.debug())
-=======
-import analysis
-
-data = analysis.load_csv('data.txt')
-print(analysis.basic_stats(0, 'debug', 0))
-print(analysis.basic_stats(data, 1, 0))
-print(analysis.basic_stats(data, 2, 0))
-print(analysis.z_score(10, analysis.basic_stats(data, 1, 0)[0],analysis.basic_stats(data, 1, 0)[3]))
-print(analysis.histo_analysis(data[0]))
-print(analysis.histo_analysis_2(data[0], 0.01, -1, 1))
-print(analysis.stdev_z_split(3.3, 0.2, 0.1, -5, 5))
-
-x = analysis.objectives(["switch", "scale", "climb"], [0,1,2], [[0,0],[1,1],[2,0]], ["0,1", "1,1", "0,5"])
->>>>>>> 18189b45b17228228e192c270c12c3cc2cb7f8cf
