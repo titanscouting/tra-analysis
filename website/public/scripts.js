c97e51d9bdf38c75f9f8c5809eea1df7e262f518
Jacob Levine <jacoblevine18@gmail.com>
2019-02-09 14:01:32 -0600

    even more bugfix

--- website/public/scripts.js
+++ website/public/scripts.js
@@ -32,8 +32,6 @@ var uiConfig = {
     alert("we will steal all of the data");
   }
 };
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
