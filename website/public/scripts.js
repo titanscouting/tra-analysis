2e779a95d2e92c402a2fe86b555ffccbde3bc9c7
Jacob Levine <jacoblevine18@gmail.com>
2019-02-09 14:00:50 -0600

    more bugfix

--- website/public/scripts.js
+++ website/public/scripts.js
@@ -34,3 +34,8 @@ var uiConfig = {
 };
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
