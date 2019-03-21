9dd4483de6b725085f71822ac8c5a24c20c5eb44
Jacob Levine <jacoblevine18@gmail.com>
2019-03-21 17:27:31 -0500

    beautified

--- data analysis/superscript.py
+++ data analysis/superscript.py
@@ -1,7 +1,7 @@
-#Titan Robotics Team 2022: Super Script
-#Written by Arthur Lu & Jacob Levine
-#Notes:
-#setup:
+# Titan Robotics Team 2022: Super Script
+# Written by Arthur Lu & Jacob Levine
+# Notes:
+# setup:
 
 __version__ = "1.0.6.000"
 
@@ -35,12 +35,12 @@ __changelog__ = """changelog:
     - added superstructure to code
 1.0.0.000:
     - added import statements (revolutionary)
-""" 
+"""
 
 __author__ = (
     "Arthur Lu <arthurlu@ttic.edu>, "
     "Jacob Levine <jlevine@ttic.edu>,"
-    )
+)
 
 import firebase_admin
 from firebase_admin import credentials
@@ -57,22 +57,24 @@ import time
 import tbarequest as tba
 import csv
 
+
 def titanservice():
-    
+
     print("[OK] loading data")
 
     start = time.time()
-    
+
     source_dir = 'data'
-    file_list = glob.glob(source_dir + '/*.csv') #supposedly sorts by alphabetical order, skips reading teams.csv because of redundancy
+    # supposedly sorts by alphabetical order, skips reading teams.csv because of redundancy
+    file_list = glob.glob(source_dir + '/*.csv')
     data = []
-    files = [fn for fn in glob.glob('data/*.csv') 
-             if not (os.path.basename(fn).startswith('scores') or os.path.basename(fn).startswith('teams') or os.path.basename(fn).startswith('match') or os.path.basename(fn).startswith('notes') or os.path.basename(fn).startswith('observationType') or os.path.basename(fn).startswith('teamDBRef'))] #scores will be handled sperately
+    files = [fn for fn in glob.glob('data/*.csv')
+             if not (os.path.basename(fn).startswith('scores') or os.path.basename(fn).startswith('teams') or os.path.basename(fn).startswith('match') or os.path.basename(fn).startswith('notes') or os.path.basename(fn).startswith('observationType') or os.path.basename(fn).startswith('teamDBRef'))]  # scores will be handled sperately
 
     for i in files:
-            data.append(analysis.load_csv(i))
+        data.append(analysis.load_csv(i))
 
-    #print(files)
+    # print(files)
 
     stats = []
     measure_stats = []
@@ -83,156 +85,168 @@ def titanservice():
 
     print("[OK] loaded data in " + str(end - start) + " seconds")
 
-    #assumes that team number is in the first column, and that the order of teams is the same across all files
-    #unhelpful comment
-    for measure in data: #unpacks 3d array into 2ds
+    # assumes that team number is in the first column, and that the order of teams is the same across all files
+    # unhelpful comment
+    for measure in data:  # unpacks 3d array into 2ds
 
         measure_stats = []
 
-        for i in range(len(measure)): #unpacks into specific teams
+        for i in range(len(measure)):  # unpacks into specific teams
 
-                    #ofbest_curve = [None]
-                    #r2best_curve = [None]
+            #ofbest_curve = [None]
+            #r2best_curve = [None]
 
-                    line = measure[i]
+            line = measure[i]
 
-                    #print(line)
+            # print(line)
 
-                    #x = list(range(len(line)))
-                    #eqs, rmss, r2s, overfit = analysis.optimize_regression(x, line, 10, 1)
+            #x = list(range(len(line)))
+            #eqs, rmss, r2s, overfit = analysis.optimize_regression(x, line, 10, 1)
 
-                    #beqs, brmss, br2s, boverfit = analysis.select_best_regression(eqs, rmss, r2s, overfit, "min_overfit")
+            #beqs, brmss, br2s, boverfit = analysis.select_best_regression(eqs, rmss, r2s, overfit, "min_overfit")
 
-                    #print(eqs, rmss, r2s, overfit)
-                    
-                    #ofbest_curve.append(beqs)
-                    #ofbest_curve.append(brmss)
-                    #ofbest_curve.append(br2s)
-                    #ofbest_curve.append(boverfit)
-                    #ofbest_curve.pop(0)
+            #print(eqs, rmss, r2s, overfit)
 
-                    #print(ofbest_curve)
+            # ofbest_curve.append(beqs)
+            # ofbest_curve.append(brmss)
+            # ofbest_curve.append(br2s)
+            # ofbest_curve.append(boverfit)
+            # ofbest_curve.pop(0)
 
-                    #beqs, brmss, br2s, boverfit = analysis.select_best_regression(eqs, rmss, r2s, overfit, "max_r2s")
+            # print(ofbest_curve)
 
-                    #r2best_curve.append(beqs)
-                    #r2best_curve.append(brmss)
-                    #r2best_curve.append(br2s)
-                    #r2best_curve.append(boverfit)
-                    #r2best_curve.pop(0)
+            #beqs, brmss, br2s, boverfit = analysis.select_best_regression(eqs, rmss, r2s, overfit, "max_r2s")
 
-                    #print(r2best_curve)
+            # r2best_curve.append(beqs)
+            # r2best_curve.append(brmss)
+            # r2best_curve.append(br2s)
+            # r2best_curve.append(boverfit)
+            # r2best_curve.pop(0)
 
-                    
-                    measure_stats.append(teams[i] + list(analysis.basic_stats(line, 0, 0)) + list(analysis.histo_analysis(line, 1, -3, 3)))
+            # print(r2best_curve)
+
+            measure_stats.append(teams[i] + list(analysis.basic_stats(
+                line, 0, 0)) + list(analysis.histo_analysis(line, 1, -3, 3)))
 
         stats.append(list(measure_stats))
         nishant = []
-            
+
     for i in range(len(scores)):
 
-                #print(scores)
+        # print(scores)
 
-                ofbest_curve = [None]
-                r2best_curve = [None]
+        ofbest_curve = [None]
+        r2best_curve = [None]
 
-                line = scores[i]
+        line = scores[i]
 
-                #print(line)
+        # print(line)
 
-                #print(line)
+        # print(line)
 
-                x = list(range(len(line)))
-                eqs, rmss, r2s, overfit = analysis.optimize_regression(x, line, 10, 1)
+        x = list(range(len(line)))
+        eqs, rmss, r2s, overfit = analysis.optimize_regression(x, line, 10, 1)
 
-                beqs, brmss, br2s, boverfit = analysis.select_best_regression(eqs, rmss, r2s, overfit, "min_overfit")
+        beqs, brmss, br2s, boverfit = analysis.select_best_regression(
+            eqs, rmss, r2s, overfit, "min_overfit")
 
-                #print(eqs, rmss, r2s, overfit)
-                    
-                ofbest_curve.append(beqs)
-                ofbest_curve.append(brmss)
-                ofbest_curve.append(br2s)
-                ofbest_curve.append(boverfit)
-                ofbest_curve.pop(0)
+        #print(eqs, rmss, r2s, overfit)
 
-                #print(ofbest_curve)
+        ofbest_curve.append(beqs)
+        ofbest_curve.append(brmss)
+        ofbest_curve.append(br2s)
+        ofbest_curve.append(boverfit)
+        ofbest_curve.pop(0)
 
-                beqs, brmss, br2s, boverfit = analysis.select_best_regression(eqs, rmss, r2s, overfit, "max_r2s")
+        # print(ofbest_curve)
 
-                r2best_curve.append(beqs)
-                r2best_curve.append(brmss)
-                r2best_curve.append(br2s)
-                r2best_curve.append(boverfit)
-                r2best_curve.pop(0)
+        beqs, brmss, br2s, boverfit = analysis.select_best_regression(
+            eqs, rmss, r2s, overfit, "max_r2s")
 
-                #print(r2best_curve)
-                    
-                z = len(scores[0]) + 1
-                nis_num = []
+        r2best_curve.append(beqs)
+        r2best_curve.append(brmss)
+        r2best_curve.append(br2s)
+        r2best_curve.append(boverfit)
+        r2best_curve.pop(0)
 
-                nis_num.append(eval(str(ofbest_curve[0])))
-                nis_num.append(eval(str(r2best_curve[0])))
+        # print(r2best_curve)
 
-                nis_num.append((eval(ofbest_curve[0]) + eval(r2best_curve[0])) / 2)
+        z = len(scores[0]) + 1
+        nis_num = []
 
-                nishant.append(teams[i] + nis_num)
-                
+        nis_num.append(eval(str(ofbest_curve[0])))
+        nis_num.append(eval(str(r2best_curve[0])))
+
+        nis_num.append((eval(ofbest_curve[0]) + eval(r2best_curve[0])) / 2)
+
+        nishant.append(teams[i] + nis_num)
+
     json_out = {}
     score_out = {}
 
     for i in range(len(teams)):
-            score_out[str(teams[i][0])] = (nishant[i])
+        score_out[str(teams[i][0])] = (nishant[i])
 
     location = db.collection(u'stats').document(u'stats-noNN')
     for i in range(len(teams)):
         general_general_stats = location.collection(teams[i][0])
-        
+
         for j in range(len(files)):
             json_out[str(teams[i][0])] = (stats[j][i])
             name = os.path.basename(files[j])
-            general_general_stats.document(name).set({'stats':json_out.get(teams[i][0])})
+            general_general_stats.document(name).set(
+                {'stats': json_out.get(teams[i][0])})
 
     for i in range(len(teams)):
-        nnum = location.collection(teams[i][0]).document(u'nishant_number').set({'nishant':score_out.get(teams[i][0])})
+        nnum = location.collection(teams[i][0]).document(
+            u'nishant_number').set({'nishant': score_out.get(teams[i][0])})
+
 
 def pulldata():
     teams = analysis.load_csv('data/teams.csv')
     scores = []
     for i in range(len(teams)):
         team_scores = []
-        #print(teams[i][0])
-        request_data_object = tba.req_team_matches(teams[i][0], 2019, "UDvKmPjPRfwwUdDX1JxbmkyecYBJhCtXeyVk9vmO2i7K0Zn4wqQPMfzuEINXJ7e5")
+        # print(teams[i][0])
+        request_data_object = tba.req_team_matches(
+            teams[i][0], 2019, "UDvKmPjPRfwwUdDX1JxbmkyecYBJhCtXeyVk9vmO2i7K0Zn4wqQPMfzuEINXJ7e5")
         json_data = request_data_object.json()
 
         for match in range(len(json_data) - 1, -1, -1):
             if json_data[match].get('winning_alliance') == "":
-                #print(json_data[match])
+                # print(json_data[match])
                 json_data.remove(json_data[match])
-            
-        json_data = sorted(json_data, key=lambda k: k.get('actual_time', 0), reverse=False)
+
+        json_data = sorted(json_data, key=lambda k: k.get(
+            'actual_time', 0), reverse=False)
         for j in range(len(json_data)):
             if "frc" + teams[i][0] in json_data[j].get('alliances').get('blue').get('team_keys'):
-                team_scores.append(json_data[j].get('alliances').get('blue').get('score'))
+                team_scores.append(json_data[j].get(
+                    'alliances').get('blue').get('score'))
             elif "frc" + teams[i][0] in json_data[j].get('alliances').get('red').get('team_keys'):
-                team_scores.append(json_data[j].get('alliances').get('red').get('score'))
+                team_scores.append(json_data[j].get(
+                    'alliances').get('red').get('score'))
         scores.append(team_scores)
 
-    with open("data/scores.csv", "w+", newline = '') as file:
-        writer = csv.writer(file, delimiter = ',')
+    with open("data/scores.csv", "w+", newline='') as file:
+        writer = csv.writer(file, delimiter=',')
         writer.writerows(scores)
 
     list_teams = teams
-    teams=db.collection('data').document('team-2022').collection("Central 2019").get()
-    full=[]
-    tms=[]
+    teams = db.collection('data').document(
+        'team-2022').collection("Central 2019").get()
+    full = []
+    tms = []
     for team in teams:
-        
+
         tms.append(team.id)
-        reports=db.collection('data').document('team-2022').collection("Central 2019").document(team.id).collection("matches").get()
+        reports = db.collection('data').document(
+            'team-2022').collection("Central 2019").document(team.id).collection("matches").get()
 
         for report in reports:
-            data=[]
-            data.append(db.collection('data').document('team-2022').collection("Central 2019").document(team.id).collection("matches").document(report.id).get().to_dict())
+            data = []
+            data.append(db.collection('data').document('team-2022').collection("Central 2019").document(
+                team.id).collection("matches").document(report.id).get().to_dict())
             full.append(data)
 
     quant_keys = []
@@ -243,21 +257,22 @@ def pulldata():
     for i in range(len(full)):
         for j in range(len(full[i])):
             for key in list(full[i][j].keys()):
-                
+
                 if "Quantitative" in key:
-                    
+
                     quant_keys.append(key)
-                    
+
                     if full[i][j].get(key).get('teamDBRef')[5:] in list_teams:
-                        
+
                         var = {}
                         measured_vars = []
-                        
+
                         for k in range(len(list(full[i][j].get(key).keys()))):
 
                             individual_keys = list(full[i][j].get(key).keys())
-                            
-                            var[individual_keys[k]] = full[i][j].get(key).get(individual_keys[k])
+
+                            var[individual_keys[k]] = full[i][j].get(
+                                key).get(individual_keys[k])
 
                         out.append(var)
 
@@ -291,7 +306,8 @@ def pulldata():
 
         for i in sorted_out:
 
-            team_index = list_teams.index(sorted_out[sorted_out.index(i)][j_list.index('teamDBRef')][5:])
+            team_index = list_teams.index(
+                sorted_out[sorted_out.index(i)][j_list.index('teamDBRef')][5:])
 
             for j in range(len(i)):
 
@@ -299,11 +315,12 @@ def pulldata():
 
         for i in range(len(big_out)):
 
-            with open('data/' + j_list[i] + '.csv', "w+", newline = '') as file:
+            with open('data/' + j_list[i] + '.csv', "w+", newline='') as file:
 
-                writer = csv.writer(file, delimiter = ',')
+                writer = csv.writer(file, delimiter=',')
                 writer.writerows(big_out[i])
-            
+
+
 def service():
 
     while True:
@@ -315,17 +332,18 @@ def service():
         print("[OK] starting calculations")
 
         fucked = False
-        
+
         for i in range(0, 5):
-            #try:
-                titanservice()
-                break
-            #except:
-                if (i != 4):
-                    print("[WARNING] failed, trying " + str(5 - i - 1) + " more times")
-                else:
-                    print("[ERROR] failed to compute data, skipping")
-                    fucked = True
+            # try:
+            titanservice()
+            break
+            # except:
+            if (i != 4):
+                print("[WARNING] failed, trying " +
+                      str(5 - i - 1) + " more times")
+            else:
+                print("[ERROR] failed to compute data, skipping")
+                fucked = True
 
         end = time.time()
         if (fucked == True):
@@ -333,15 +351,16 @@ def service():
             break
 
         else:
-            
+
             print("[OK] finished calculations")
 
         print("[OK] waiting: " + str(300 - (end - start)) + " seconds" + "\n")
 
-        time.sleep(300 - (end - start)) #executes once every 5 minutes
+        time.sleep(300 - (end - start))  # executes once every 5 minutes
+
 
 warnings.simplefilter("ignore")
-#Use a service account
+# Use a service account
 try:
     cred = credentials.Certificate('keys/firebasekey.json')
 except:
@@ -350,5 +369,5 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-service() #finally we write something that isn't a function definition
-#titanservice()
+service()  # finally we write something that isn't a function definition
+# titanservice()
