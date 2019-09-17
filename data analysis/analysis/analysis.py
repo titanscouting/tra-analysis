f63c473166e3c3901cb14e02a3702a4e047f286a
ltcptgeneral <learthurgo@gmail.com>
2019-09-17 12:21:44 -0500

    analysis.py v 1.1.0.006

--- data analysis/analysis/analysis.py
+++ data analysis/analysis/analysis.py
@@ -7,10 +7,12 @@
 #   current benchmark of optimization: 1.33 times faster
 # setup:
 
-__version__ = "1.1.0.005"
+__version__ = "1.1.0.006"
 
 # changelog should be viewed using print(analysis.__changelog__)
 __changelog__ = """changelog:
+1.1.0.006:
+    - removed useless try statements
 1.1.0.005:
     - removed impossible outcomes
 1.1.0.004:
@@ -158,11 +160,9 @@ class error(ValueError):
 
 def _init_device(setting, arg):  # initiates computation device for ANNs
     if setting == "cuda":
-        try:
-            return torch.device(setting + ":" + str(arg) if torch.cuda.is_available() else "cpu")
+        return torch.device(setting + ":" + str(arg) if torch.cuda.is_available() else "cpu")
     elif setting == "cpu":
-        try:
-            return torch.device("cpu")
+        return torch.device("cpu")
     else:
         raise error("specified device does not exist")
 
