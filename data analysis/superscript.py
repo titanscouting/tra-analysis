a92d28ace71e6046a1a0c567bc26ba93a25ea99d
art <learthurgo@gmail.com>
2020-02-18 11:31:20 -0600

    superscript v 0.0.0.001

--- data analysis/superscript.py
+++ data analysis/superscript.py
@@ -1,1 +1,27 @@
-from analysis import analysis
+# Titan Robotics Team 2022: Superscript Script
+# Written by Arthur Lu & Jacob Levine
+# Notes:
+# setup:
+
+__version__ = "0.0.0.001"
+
+# changelog should be viewed using print(analysis.__changelog__)
+__changelog__ = """changelog:
+    0.0.0.001:
+        - created script
+        - added analysis, numba, numpy imports
+"""
+
+__author__ = (
+    "Arthur Lu <learthurgo@gmail.com>",
+    "Jacob Levine <jlevine@imsa.edu>",
+)
+
+__all__ = [
+]
+
+# imports:
+
+from analysis import analysis as an
+from numba import jit
+import numpy as np
