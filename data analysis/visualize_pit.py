3ab1d0f50abdfce6b9e27fe9641da726f29eec85
ltcptgeneral <35508619+ltcptgeneral@users.noreply.github.com>
2020-05-01 16:15:07 -0500

    converted space indentation to tab indentation

--- data analysis/visualize_pit.py
+++ data analysis/visualize_pit.py
@@ -8,20 +8,20 @@ import pymongo
 
 # %%
 def get_pit_variable_data(apikey, competition):
-    client = pymongo.MongoClient(apikey)
-    db = client.data_processing
-    mdata = db.team_pit
-    out = {}
-    return mdata.find()
+	client = pymongo.MongoClient(apikey)
+	db = client.data_processing
+	mdata = db.team_pit
+	out = {}
+	return mdata.find()
 
 
 # %%
 def get_pit_variable_formatted(apikey, competition):
-    temp = get_pit_variable_data(apikey, competition)
-    out = {}
-    for i in temp:
-        out[i["variable"]] = i["data"]
-    return out
+	temp = get_pit_variable_data(apikey, competition)
+	out = {}
+	for i in temp:
+		out[i["variable"]] = i["data"]
+	return out
 
 
 # %%
@@ -40,16 +40,16 @@ i = 0
 
 for variable in pit:
 
-    ax[i].hist(pit[variable])
-    ax[i].invert_xaxis()
+	ax[i].hist(pit[variable])
+	ax[i].invert_xaxis()
 
-    ax[i].set_xlabel('')
-    ax[i].set_ylabel('Frequency')
-    ax[i].set_title(variable)
+	ax[i].set_xlabel('')
+	ax[i].set_ylabel('Frequency')
+	ax[i].set_title(variable)
 
-    plt.yticks(np.arange(len(pit[variable])))
+	plt.yticks(np.arange(len(pit[variable])))
 
-    i+=1
+	i+=1
 
 plt.show()
 
