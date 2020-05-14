132e809ab5738714feea5c4a796954fe63cf0f78
Arthur <learthurgo@gmail.com>
2020-05-14 16:52:42 -0500

    more changes to visualize_pit.py
    Signed-off-by: Arthur <learthurgo@gmail.com>

--- data-analysis/visualize_pit.py
+++ data-analysis/visualize_pit.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import data as d
+import matplotlib.pyplot as plt
+import numpy as np
 import pymongo
 
 def get_pit_variable_data(apikey, competition):
@@ -16,26 +17,25 @@ def get_pit_variable_formatted(apikey, competition):
 		out[i["variable"]] = i["data"]
 	return out
 
-pit = get_pit_variable_formatted("mongodb+srv://api-user-new:titanscout2022@2022-scouting-4vfuu.mongodb.net/test?authSource=admin&replicaSet=2022-scouting-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", "2020ilch")
+def main(apikey, competition):
 
-import matplotlib.pyplot as plt
-import numpy as np
+	pit = get_pit_variable_formatted(apikey, competition)
 
-fig, ax = plt.subplots(1, len(pit), sharey=True, figsize=(80,15))
+	fig, ax = plt.subplots(1, len(pit), sharey=True, figsize=(80,15))
 
-i = 0
+	i = 0
 
-for variable in pit:
+	for variable in pit:
 
-	ax[i].hist(pit[variable])
-	ax[i].invert_xaxis()
+		ax[i].hist(pit[variable])
+		ax[i].invert_xaxis()
 
-	ax[i].set_xlabel('')
-	ax[i].set_ylabel('Frequency')
-	ax[i].set_title(variable)
+		ax[i].set_xlabel('')
+		ax[i].set_ylabel('Frequency')
+		ax[i].set_title(variable)
 
-	plt.yticks(np.arange(len(pit[variable])))
+		plt.yticks(np.arange(len(pit[variable])))
 
-	i+=1
+		i+=1
 
-plt.show()
+	plt.show()
