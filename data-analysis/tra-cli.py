bb8253df3987d73c46d51f15adf3e7463c0031ee
Arthur Lu <learthurgo@gmail.com>
2020-06-10 21:47:54 +0000

    fixed more bugs with tra-cli.py
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- data-analysis/tra-cli.py
+++ data-analysis/tra-cli.py
@@ -11,16 +11,15 @@ parser = argparse.ArgumentParser(prog = "TRA")
 subparsers = parser.add_subparsers(title = "command", metavar = "C", help = "//commandhelp//")
 
 parser_start = subparsers.add_parser("start", help = "//starthelp//")
-parser_start.add_argument("target(s)", metavar = "T", nargs = "*", choices = task_map.keys())
+parser_start.add_argument("targets", metavar = "T", nargs = "*", choices = task_map.keys())
 parser_start.set_defaults(which = "start")
 
 parser_stop = subparsers.add_parser("stop", help = "//stophelp//")
-parser_stop.add_argument("target(s)", metavar = "T", nargs = "*", choices = task_map.keys())
+parser_stop.add_argument("targets", metavar = "T", nargs = "*", choices = task_map.keys())
 parser_stop.set_defaults(which = "stop")
 
 parser_status = subparsers.add_parser("status", help = "//stophelp//")
-parser_status.add_argument("target(s)", metavar = "T", nargs = "*", choices = status_map.keys())
+parser_status.add_argument("targets", metavar = "T", nargs = "*", choices = status_map.keys())
 parser_status.set_defaults(which = "status")
 
-options = parser.parse_args()
-print(options)
+args = parser.parse_args()
