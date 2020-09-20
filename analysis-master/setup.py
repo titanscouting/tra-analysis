fa7216d4e05bebba39f39eed949c67c78959aca1
Arthur Lu <learthurgo@gmail.com>
2020-09-20 00:50:14 +0000

    modified setup.py for analysis package v 2.1.0
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- analysis-master/setup.py
+++ analysis-master/setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", 'r') as file:
 
 setuptools.setup(
 	name="tra_analysis",
-	version="2.0.3",
+	version="2.1.0",
 	author="The Titan Scouting Team",
 	author_email="titanscout2022@gmail.com",
 	description="Analysis package developed by Titan Scouting for The Red Alliance",
