54c1d5cf5a0ba61ff4c8880939edf2d0f000bd49
AGawde05 <agawde05@gmail.com>
2020-11-02 16:49:12 -0600

    feat: gui with placeholders

--- data-analysis/main.py
+++ data-analysis/main.py
@@ -1,41 +1,22 @@
-from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager , Screen
-from kivy.animation import Animation
-from hoverable import HoverBehavior
-from kivy.uix.image import Image
-from kivy.uix.behaviors import ButtonBehavior
-import json
-from datetime import datetime
-import glob
-from pathlib import Path
-import random
-
-import superscript as ss
-
-Builder.load_file('design.kv')
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+# import superscript as ss
 
 class HomeScreen(Screen):
-    # def sign_up(self):
-    #     self.manager.transition.direction = "left"
-    #     self.manager.current = "sign_up_screen"
-    
-    # def login(self, uname, pword):
-    #     with open ("users.json") as file:
-    #         users = json.load(file)
-    #     if uname in users and users[uname]["password"] == pword:
-    #         self.manager.transition.direction = "left"
-    #         self.manager.current = "login_screen_success"
-    #     else:
-    #         self.ids.login_wrong.text = "Incorrect Username or Password"
-
-
-class RootWidget(ScreenManager):
+    pass
+class SettingsScreen(Screen):
+    pass
+class InfoScreen(Screen):
     pass
 
-class MainApp(App):
+class MyApp(MDApp):
     def build(self):
-        return RootWidget()
+        self.theme_cls.primary_palette = "Red"
+        return Builder.load_file("design.kv")
+
+
 
 if __name__ == "__main__":
-    MainApp().run()
+    MyApp().run()
