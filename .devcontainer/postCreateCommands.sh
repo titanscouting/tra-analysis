7f03b8546aa8da8c59e2e1c7f03d253dd0b96cc4
Dev Singh <dev@devksingh.com>
2020-11-03 00:56:11 +0000

    fix: docker build?

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -4,11 +4,7 @@ apt-get install software-properties-common
 apt-add-repository universe
 apt update -y
 apt upgrade -y
-apt-get install python3.7-dev -y
-apt install -y libc6 build-essential libgl1-mesa-glx libglib2.0-0 libgstreamer1.0-0 libsdl2-2.0.0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0
-apt install python3-pip -y
-apt install -y build-essential
-apt install git -y
+apt-get install -y python3.7 python3.7-dev git python3-pip python3-kivy libc6 build-essential libgl1-mesa-glx libglib2.0-0 libgstreamer1.0-0 libsdl2-2.0.0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0
 pip install -r /workspaces/red-allsiance-analysis/data-analysis/requirements.txt
 pip install -r /workspaces/red-alliance-analysis/analysis-master/requirements.txt
 pip install --no-cache-dir pylint
