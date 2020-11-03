c62d3659ec14837010ee32880c8448b96b9a1f7a
Dev Singh <dev@devksingh.com>
2020-11-03 01:11:39 +0000

    @ltcptgeneral can't spell

--- .devcontainer/postCreateCommands.sh
+++
@@ -1,5 +0,0 @@
-source ~/.bashrc
-shopt -s expand_aliases
-pip install -r /workspaces/red-allsiance-analysis/data-analysis/requirements.txt
-pip install -r /workspaces/red-alliance-analysis/analysis-master/requirements.txt
-pip install --no-cache-dir pylint
