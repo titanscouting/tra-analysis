087fb413e776fda711e4dd0393d7f7c1e7259219
Arthur Lu <learthurgo@gmail.com>
2021-01-31 02:03:09 +0000

    incremented version to rc.1 (release candidate 1)
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- analysis-master/tra_analysis/__init__.py
+++ analysis-master/tra_analysis/__init__.py
@@ -7,7 +7,7 @@
 #    current benchmark of optimization: 1.33 times faster
 # setup:
 
-__version__ = "3.0.0-alpha.4"
+__version__ = "3.0.0-rc1"
 
 # changelog should be viewed using print(analysis.__changelog__)
 __changelog__ = """changelog:
