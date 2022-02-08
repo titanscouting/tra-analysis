6647dcfd726988e06f27d1aaf71a293a106cdf31
Arthur Lu <learthurgo@gmail.com>
2022-02-08 07:29:47 +0000

    generalized keyword argument handling for: Clustering.py, CorrelationTest.py, KNN.py, NaiveBayes.py

--- analysis-master/tra_analysis/Clustering.py
+++ analysis-master/tra_analysis/Clustering.py
@@ -4,10 +4,12 @@
 #    this should be imported as a python module using 'from tra_analysis import Clustering'
 # setup:
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 # changelog should be viewed using print(analysis.__changelog__)
 __changelog__ = """changelog:
+	2.0.2:
+		- generalized optional args to **kwargs
 	2.0.1:
 		- added normalization preprocessing to clustering, expects instance of sklearn.preprocessing.Normalizer()
 	2.0.0:
@@ -30,32 +32,32 @@ __all__ = [
 
 import sklearn
 
-def kmeans(data, normalizer = None, n_clusters=8, init="k-means++", n_init=10, max_iter=300, tol=0.0001, precompute_distances="auto", verbose=0, random_state=None, copy_x=True, n_jobs=None, algorithm="auto"):
+def kmeans(data, normalizer = None, **kwargs):
 
 	if  normalizer != None:
 		data = normalizer.transform(data)
 
-	kernel = sklearn.cluster.KMeans(n_clusters = n_clusters, init = init, n_init = n_init, max_iter = max_iter, tol = tol, precompute_distances = precompute_distances, verbose = verbose, random_state = random_state, copy_x = copy_x, n_jobs = n_jobs, algorithm = algorithm)
+	kernel = sklearn.cluster.KMeans(**kwargs)
 	kernel.fit(data)
 	predictions = kernel.predict(data)
 	centers = kernel.cluster_centers_
 
 	return centers, predictions
 
-def dbscan(data, normalizer=None, eps=0.5, min_samples=5, metric='euclidean', metric_params=None, algorithm='auto', leaf_size=30, p=None, n_jobs=None):
+def dbscan(data, normalizer=None, **kwargs):
 
 	if  normalizer != None:
 		data = normalizer.transform(data)
 
-	model = sklearn.cluster.DBSCAN(eps = eps, min_samples = min_samples, metric = metric, metric_params = metric_params, algorithm = algorithm, leaf_size = leaf_size, p = p, n_jobs = n_jobs).fit(data)
+	model = sklearn.cluster.DBSCAN(**kwargs).fit(data)
 
 	return model.labels_
 
-def spectral(data, normalizer=None, n_clusters=8, eigen_solver=None, n_components=None, random_state=None, n_init=10, gamma=1.0, affinity='rbf', n_neighbors=10, eigen_tol=0.0, assign_labels='kmeans', degree=3, coef0=1, kernel_params=None, n_jobs=None, verbose=False):
+def spectral(data, normalizer=None, **kwargs):
 
 	if  normalizer != None:
 		data = normalizer.transform(data)
 
-	model = sklearn.cluster.SpectralClustering(n_clusters = n_clusters, eigen_solver = eigen_solver, n_components = n_components, random_state = random_state, n_init = n_init, gamma = gamma, affinity = affinity, n_neighbors = n_neighbors, eigen_tol = eigen_tol, assign_labels = assign_labels, degree = degree, coef0 = coef0, kernel_params = kernel_params, n_jobs = n_jobs).fit(data)
+	model = sklearn.cluster.SpectralClustering(**kwargs).fit(data)
 
 	return model.labels_
--- analysis-master/tra_analysis/KNN.py
+++ analysis-master/tra_analysis/KNN.py
@@ -4,9 +4,11 @@
 #    this should be imported as a python module using 'from tra_analysis import KNN'
 # setup:
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 __changelog__ = """changelog:
+	1.0.2:
+		- generalized optional args to **kwargs
 	1.0.1:
 		- optimized imports
 	1.0.0:
@@ -27,19 +29,19 @@ __all__ = [
 import sklearn
 from . import ClassificationMetric, RegressionMetric
 
-def knn_classifier(data, labels, n_neighbors = 5, test_size = 0.3, algorithm='auto', leaf_size=30, metric='minkowski', metric_params=None, n_jobs=None, p=2, weights='uniform'): #expects *2d data and 1d labels post-scaling
+def knn_classifier(data, labels, n_neighbors = 5, test_size = 0.3, **kwargs): #expects *2d data and 1d labels post-scaling
 
 	data_train, data_test, labels_train, labels_test = sklearn.model_selection.train_test_split(data, labels, test_size=test_size, random_state=1)
-	model = sklearn.neighbors.KNeighborsClassifier(n_neighbors = n_neighbors, weights = weights, algorithm = algorithm, leaf_size = leaf_size, p = p, metric = metric, metric_params = metric_params, n_jobs = n_jobs)
+	model = sklearn.neighbors.KNeighborsClassifier(n_neighbors = n_neighbors, **kwargs)
 	model.fit(data_train, labels_train)
 	predictions = model.predict(data_test)
 
 	return model, ClassificationMetric(predictions, labels_test)
 
-def knn_regressor(data, outputs, n_neighbors = 5, test_size = 0.3, weights = "uniform", algorithm = "auto", leaf_size = 30, p = 2, metric = "minkowski", metric_params = None, n_jobs = None):
+def knn_regressor(data, outputs, n_neighbors = 5, test_size = 0.3, **kwargs):
 
 	data_train, data_test, outputs_train, outputs_test = sklearn.model_selection.train_test_split(data, outputs, test_size=test_size, random_state=1)
-	model = sklearn.neighbors.KNeighborsRegressor(n_neighbors = n_neighbors, weights = weights, algorithm = algorithm, leaf_size = leaf_size, p = p, metric = metric, metric_params = metric_params, n_jobs = n_jobs)
+	model = sklearn.neighbors.KNeighborsRegressor(n_neighbors = n_neighbors, **kwargs)
 	model.fit(data_train, outputs_train)
 	predictions = model.predict(data_test)
 
